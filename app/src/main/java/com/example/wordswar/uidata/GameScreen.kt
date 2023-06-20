b7d8967eedcfde95ae59adc45669c748f4dc12b5
KingsMunene <munenekings2@gmail.com>
2023-06-20 14:03:25 +0300

    outlined text composable changes

--- app/src/main/java/com/example/wordswar/uidata/GameScreen.kt
+++ app/src/main/java/com/example/wordswar/uidata/GameScreen.kt
@@ -1,14 +1,21 @@
 package com.example.wordswar.uidata
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.wordswar.R
 
 @Composable
@@ -26,12 +33,19 @@ fun GameScreen() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GameLayout() {
-    Column(modifier = Modifier) {
+    Column(modifier = Modifier
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(stringResource(R.string.word_count))
         Text(text = "nlmeo")
         Text(stringResource(R.string.game_instructions))
-        OutlinedTextField(value = "", onValueChange = {},
-            label = { Text(stringResource(R.string.word_textfield))})
+        OutlinedTextField(value = "",
+            onValueChange = {},
+            label = { Text(stringResource(R.string.word_textfield))},
+            singleLine = true,
+            colors = TextFieldDefaults.textFieldColors(containerColor = colorScheme.surface),
+            shape = shapes.medium)
 
     }
 }
