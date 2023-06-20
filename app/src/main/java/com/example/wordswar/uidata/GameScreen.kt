24ae6ffbc0701d75393ede74edb45728afe7bc33
KingsMunene <munenekings2@gmail.com>
2023-06-20 17:58:41 +0300

    Buttons and GameLayout part of the composation

--- app/src/main/java/com/example/wordswar/uidata/GameScreen.kt
+++ app/src/main/java/com/example/wordswar/uidata/GameScreen.kt
@@ -1,30 +1,63 @@
 package com.example.wordswar.uidata
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.MaterialTheme.shapes
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.wordswar.R
 
 @Composable
 fun GameScreen() {
-    Column(modifier = Modifier) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
         Text(stringResource(R.string.game_title),
             style = MaterialTheme.typography.titleMedium)
 
         GameLayout()
+
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier.fillMaxWidth()) {
+                Text(stringResource(R.string.submit_button))
+            }
+            OutlinedButton(onClick = { /*TODO*/ },
+                modifier = Modifier.fillMaxWidth()) {
+                Text(stringResource(R.string.skip_button))
+            }
+
+            Text(stringResource(R.string.game_score))
+        }
+
 
     }
 }
@@ -33,21 +66,43 @@ fun GameScreen() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GameLayout() {
-    Column(modifier = Modifier
-        .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(stringResource(R.string.word_count))
-        Text(text = "nlmeo")
-        Text(stringResource(R.string.game_instructions))
-        OutlinedTextField(value = "",
-            onValueChange = {},
-            label = { Text(stringResource(R.string.word_textfield))},
-            singleLine = true,
-            colors = TextFieldDefaults.textFieldColors(containerColor = colorScheme.surface),
-            shape = shapes.medium)
 
+    Card() {
+        Column(modifier = Modifier
+            .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier
+                    .clip(shapes.medium)
+                    .padding(horizontal = 10.dp, vertical = 4.dp)
+                    .align(alignment = Alignment.End)
+                    .background(colorScheme.surfaceTint),
+                text =  stringResource(R.string.word_count),
+                color = colorScheme.onPrimary,
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Text(text = "nlmeo")
+            Text(stringResource(R.string.game_instructions))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text(stringResource(R.string.word_textfield))},
+                singleLine = true,
+                colors = TextFieldDefaults.textFieldColors(containerColor = colorScheme.surface),
+                shape = shapes.medium,
+                isError = false,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ), keyboardActions = KeyboardActions(
+                    onDone = {}
+                )
+            )
+
+        }
     }
+
 }
 
 
